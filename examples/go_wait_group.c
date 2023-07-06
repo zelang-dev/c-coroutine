574d949f47a81af5ddecb538f915df55548378af
TheTechsTech <technoexpressnet@gmail.com>
2023-07-06 14:27:41 -0400

    remove tests, update wait, no direct call needed
    - testing shows under Linux mixing scheduler coroutines with direct `co_switch` to coroutine and bypassing scheduler, causing stack overflows, needs refactoring, remove for now.

--- examples/go_wait_group.c
+++ examples/go_wait_group.c
@@ -8,8 +8,12 @@ void *worker(void *arg)
 
     co_sleep(1000);
     printf("Worker %d done\n", id);
+    if (id == 4)
+        return (void *)32;
+    else if (id == 3)
+        return (void *)"hello world\0";
 
-    return (id == 4) ? (void *)32 : 0;
+    return 0;
 }
 
 int co_main(int argc, char **argv)
@@ -23,5 +27,6 @@ int co_main(int argc, char **argv)
     co_ht_result_t *wgr = co_wait(wg);
 
     printf("\nWorker # %d returned: %d\n", cid[2], co_group_get_result(wgr, cid[2]).integer);
+    printf("\nWorker # %d returned: %s\n", cid[1], co_group_get_result(wgr, cid[1]).string);
     return 0;
 }
