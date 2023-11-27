7aea0d528b67941d64006e9c503bc4c3f8e047f4
Lawrence Stubbs <lstubbs@techno.express>
2023-11-27 18:52:04 -0500

    build: update .cl_32.bat and .cl_64.bat

--- .cl_64.bat
+++ .cl_64.bat
@@ -1,7 +1,5 @@
 @echo off
-if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools" (
-    %comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-) else if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools" (
+if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools" (
     %comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 ) else if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community" (
     %comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
@@ -9,4 +7,6 @@ if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools" (
     %comspec% /k "C:\Program Files\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 ) else if EXIST "C:\Program Files\Microsoft Visual Studio\2022\Community" (
     %comspec% /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+) else if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools" (
+    %comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 )
