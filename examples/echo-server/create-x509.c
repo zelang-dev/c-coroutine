7b2b1c91eb92b203ff82f05f2191826abb288db3
Lawrence Stubbs <lstubbs@techno.express>
2024-08-05 17:18:24 -0400

    bug fix no OPENSSL_Applink in example

--- examples/echo-server/create-x509.c
+++ examples/echo-server/create-x509.c
@@ -1,5 +1,5 @@
 
-#include "../../include/uv.h"
+#include "uv.h"
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -8,7 +8,7 @@
 #include <openssl/x509.h>
 
 #if defined(_WIN32) || defined(_WIN64)
-    #include "../../include/compat/unistd.h"
+    #include "compat/unistd.h"
 #else
     #include <unistd.h>
 #endif
