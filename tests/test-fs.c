cdc0d3165db4aa948bd846ed4096b56cf14e34c9
TheTechsTech <technoexpressnet@gmail.com>
2025-04-15 14:43:05 -0400

    remove dup tests, issues still with sleep & yield
    ** single tests works fine, multi not resuming from yield, not actually calling sub coroutine before before libuv file system function call, it switches and return results, while remaining test just stall displaying/spinning `coro_wait_system` **

--- tests/test-fs.c
+++ tests/test-fs.c
@@ -7,7 +7,7 @@ char *path = "write.tmp";
 
 void_t worker(params_t args) {
     ASSERT_WORKER(is_str_eq("hello world", args->char_ptr));
-    sleepfor(1000);
+    sleepfor(500);
     return "done";
 }
 
@@ -31,7 +31,7 @@ TEST(fs_close) {
 }
 
 void_t worker2(params_t args) {
-    sleepfor(1);
+    sleepfor(10);
     return "hello world";
 }
 
@@ -44,6 +44,10 @@ TEST(fs_write_read) {
     ASSERT_STR("bla", fs_read(fd, 6));
     ASSERT_EQ(0, fs_close(fd));
     ASSERT_EQ(0, fs_unlink(path));
+    while (!result_is_ready(res)) {
+        yielding();
+    }
+
     ASSERT_TRUE(result_is_ready(res));
     ASSERT_STR(result_for(res).char_ptr, "hello world");
 
@@ -53,18 +57,22 @@ TEST(fs_write_read) {
 void_t worker3(params_t args) {
     ASSERT_WORKER(($size(args) == 2));
     sleepfor(args[0].u_int);
-    ASSERT_WORKER((args[0].u_int == 1));
+    ASSERT_WORKER((args[0].u_int == 25));
     ASSERT_WORKER(is_str_eq("worker", args[1].char_ptr));
     return "finish";
 }
 
 TEST(fs_open) {
-    rid_t res = go(worker3, 2, 1, "worker");
+    rid_t res = go(worker3, 2, 25, "worker");
     ASSERT_EQ(fs_open("does_not_exist", O_RDONLY, 0), UV_ENOENT);
     printf("\nWill indicate memory leak at exit, no `fs_close()`.\n");
     uv_file fd = fs_open(__FILE__, O_RDONLY, 0);
     ASSERT_TRUE((fd > 0));
     ASSERT_STR("/******hello  world******/", str_trim(fs_read(fd, 27), 26));
+    while (!result_is_ready(res)) {
+        yielding();
+    }
+
     ASSERT_TRUE(result_is_ready(res));
     ASSERT_STR(result_for(res).char_ptr, "finish");
 
